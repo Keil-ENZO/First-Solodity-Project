0eb3bdcfcc471b4014de8f023457f37da820bba5
Keil-ENZO <enzo.keil06@icloud.com>
2024-10-26 19:25:57 +0200

    first commit

---
+++ contracts/test.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+contract Test {
+
+    address public owner;
+    uint public eth;
+
+    uint256 public date = 1 days;
+
+    function setAdress(address _owner) public {
+
+            owner = _owner;
+    }
+    
+    function setWalletEth(uint _eth) public {
+        eth = _eth;
+    }
+
+ 
+}
