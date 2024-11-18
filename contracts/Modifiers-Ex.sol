627e96804b7b0bbc5341adf134eb6c8d2ce23e88
Keil-ENZO <enzo.keil06@icloud.com>
2024-11-18 18:34:31 +0100

    modifiers-exo

---
+++ contracts/Modifiers-Ex.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract PausableToken {
+    address public owner;
+    bool public paused;
+    mapping(address => uint) public balances;
+
+    constructor() {
+        owner = msg.sender;
+        paused = false;
+        balances[owner] = 1000;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not owner");
+
+        _;
+    }
+
+    modifier notPaused() {
+        require(paused == false, "Is paused");
+
+        _;
+    }
+
+    function pause() public onlyOwner { 
+        paused = true;
+    }
+
+
+    function unpause() public onlyOwner {
+        paused = false;
+    }
+
+    function transfer(address to, uint amount) public notPaused {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+}
--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -4,6 +4,8 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract Twitter {
 
+    uint256 constant minLength = 5; 
+    uint256 constant maxLength = 280;
 
     struct Tweet {
         address author;
@@ -15,7 +17,10 @@ contract Twitter {
     mapping(address => Tweet[]) public tweets;
 
 
+
     function createTweet(string memory _tweet) public {
+        require(bytes(_tweet).length >= minLength, "You tweet is too short" );
+        require(bytes(_tweet).length <= maxLength, "You tweet is too long");
         
         Tweet memory newTweet = Tweet({
             author: msg.sender,
